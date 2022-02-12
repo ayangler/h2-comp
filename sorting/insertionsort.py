7d653adba07710c2490f7962961e74716525bc39
angler <angler.ghy@gmail.com>
2022-02-12 17:01:19 +0800

    reformat insertion sort

--- sorting/insertionsort.py
+++ sorting/insertionsort.py
@@ -1,8 +1,8 @@
 def insertion_sort(array):
     cmp, swp = 0, 0
     for i in range(len(array)):
-        for j in range(i,0,-1):
+        for j in range(i, 0, -1):
             cmp += 1
-            if array[j] < array[j-1]:
+            if array[j] < array[j - 1]:
                 swp += 1
-                array[j], array[j-1] = array[j-1], array[j]
+                array[j], array[j - 1] = array[j - 1], array[j]
